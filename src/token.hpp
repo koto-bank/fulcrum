d59c4248e02b17a49c8ebcba96d8227e23f0a70d
alex-eg <alexey.e.egorov@gmail.com>
2023-05-29 21:39:16 +0300

    Improve tokens' constness

--- src/token.hpp
+++ src/token.hpp
@@ -83,7 +83,7 @@ struct RParen final : public Token {
 
 struct Id final : public Token {
     Id(const std::string &id);
-    std::string id;
+    const std::string id;
 };
 
 struct Keyword final : public Token {
@@ -94,15 +94,16 @@ struct Keyword final : public Token {
 struct BooleanLiteral final : public Token {
     BooleanLiteral(bool value);
 
-    bool value;
+    const bool value;
 };
 
 struct IntegerLiteral final : public Token {
-    IntegerLiteral(uint64_t value, bool isSigned, uint32_t bits);
+    IntegerLiteral(int64_t value, uint32_t bits);
+    IntegerLiteral(uint64_t value, uint32_t bits);
 
-    std::variant<uint64_t, int64_t> value;
-    bool isSigned;
-    uint32_t bits;
+    const std::variant<uint64_t, int64_t> value;
+    const bool isSigned;
+    const uint32_t bits;
 
     template <typename T>
     T get() {
@@ -117,14 +118,14 @@ struct IntegerLiteral final : public Token {
 struct CharLiteral final : public Token {
     CharLiteral(uint8_t value);
 
-    uint8_t value;
+    const uint8_t value;
 };
 
 struct FloatLiteral final : public Token {
     FloatLiteral(double value, uint32_t bits);
 
-    double value;
-    uint32_t bits;
+    const double value;
+    const uint32_t bits;
 
     template <typename T>
     T get() {
@@ -134,7 +135,7 @@ struct FloatLiteral final : public Token {
 
 struct StringLiteral final : Token {
     StringLiteral(const std::string &contents);
-    std::string contents;
+    const std::string contents;
 };
 
 struct EndOfFile final : Token {
